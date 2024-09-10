bbdf3758b4b7fed90220385be65d9cc7d8e1e797
creepybrow <144290004+creepybrow@users.noreply.github.com>
2024-09-10 09:26:54 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Navbar from "./components/Navbar"
+import Model from "./components/Model"
+
+const App = () =>{
+      return (
+       <main>
+        <Navbar /> 
+        <Hero />
+        <Highlights/>
+        <Model />
+       </main>
+      )
+    }
+
+export default App
---
+++ src/components/ModelView.jsx
@@ -0,0 +1,45 @@
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
+import Lights from "./Lights"
+import { Suspense } from "react"
+import IPhone from './IPhone';
+import * as THREE from 'three'
+
+const ModelView = ({index, groupRef, gsapType, controlRef,
+    setRotationSize, size, item}) => {
+  return (
+    <View
+    index={index}
+    id={gsapType}
+    className=
+    {`w-full h-full ${index ===2} ? 'right-[100%]: ''`}>
+    {/*ambient light*/}
+    <ambientLight intensity={0.3}/>
+    <PerspectiveCamera makeDefault position={[0 ,0,
+    4]} />
+    <Lights />
+    <OrbitControls
+    makeDefault
+    ref={controlRef}
+    enableZoom={false}
+    enablePan={false}
+    rotateSpeed={0.4}
+    target={new THREE.Vector3(0,0,0)}
+    onEnd={() => setRotationState(controlRef.current.
+      getAzimuthalAngle())}
+      item={item}
+      size={size}
+    />
+    <group ref={groupRef} name={`${index === 1 ? 
+      'small' : 'large'}`} position={[0,0,0]}>
+    <Suspense fallback={<div>Loading</div>}>
+    <IPhone 
+    scale={ index === 1 ? [15,15,15] : [17,17,17]}/>
+    <Lights/>
+    </Suspense>
+    </group>
+    </View>
+
+  )
+}
+
+export default ModelView
